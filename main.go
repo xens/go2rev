21155567bba397c9cd2a9e0c91e33af483def4e4
Romain Aviolat <r.aviolat@gmail.com>
2020-05-16 08:59:28 +0200

    add initial webserver

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "net/http"
+)
+
+// create a handler struct
+type HttpHandler struct{}
+
+// implement `ServeHTTP` method on `HttpHandler` struct
+func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+
+    // create response binary data
+    data := []byte("Hello World! ❤️") // slice of bytes
+
+    // write `data` to response
+    res.Write(data)
+}
+
+func main() {
+
+    // create a new handler
+    handler := HttpHandler{}
+
+    // listen and serve
+    http.ListenAndServe(":9000", handler)
+}
